1a9b72bc6ff9bbe9311f366ddd8b25bff3162dbd
Tomasz Goralski <goralski.tomasz@qiagen.com>
2015-07-08 14:22:09 +0200

    added karma gradle running grunt tasks

--- test/calculator-test.js
+++ test/calculator-test.js
@@ -1,6 +1,30 @@
 //var expect = require('chai').expect;
-
 var expect = chai.expect;
+
+if (!Function.prototype.bind) {
+    Function.prototype.bind = function(oThis) {
+        if (typeof this !== 'function') {
+            // closest thing possible to the ECMAScript 5
+            // internal IsCallable function
+            throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+        }
+
+        var aArgs   = Array.prototype.slice.call(arguments, 1),
+            fToBind = this,
+            fNOP    = function() {},
+            fBound  = function() {
+                return fToBind.apply(this instanceof fNOP
+                        ? this
+                        : oThis,
+                    aArgs.concat(Array.prototype.slice.call(arguments)));
+            };
+
+        fNOP.prototype = this.prototype;
+        fBound.prototype = new fNOP();
+
+        return fBound;
+    };
+}
 
 describe('Calculator', function () {
     // Mocha's before() function runs once before the rest of the tests start
@@ -40,6 +64,44 @@ describe('Calculator', function () {
             // called upon which would make add() fail.
             var filterSpy = sinon.spy(this.calculator, '_filter');
             this.calculator.add(2, 5);
+            // Chai + Sinon here.
+            expect(filterSpy).to.have.been.called;
+            // When creating a spy or stub to wrap a function, you'll want
+            // to make sure you restore the original function back at the
+            // end of your test case
+            filterSpy.restore();
+        });
+    });
+
+    describe('Subsctract', function () {
+        it('should throw an error if a non-numeric value is used', function () {
+            // Chai's expect function will take any value, including a function
+            // but in our case, we want it to throw an error. If we just said
+            // this.calculator.add(2, 'a'), the error would throw before `expect`
+            // could handle it. This is why we create a new function using bind
+            // passing in our specified arguments and let Chai catch the error.
+            expect(this.calculator.substract.bind(this.calculator, 2, 'a')).to.throw();
+        });
+
+        it('should throw an error if less than 2 values are provided', function () {
+            expect(this.calculator.substract.bind(this.calculator, 2)).to.throw();
+        });
+
+        it('should add a series of number arguments together', function () {
+            // This test case reads exactly how you would expect
+            expect(this.calculator.substract(2, 5)).to.equal(-3); // passes
+        });
+
+        // All functions like add, subtract etc, should filter the non-numbers
+        // before operating on them. We can check to see that the filter was called
+        // using a spy
+        it('should filter the input', function () {
+            // Create spy on the filter function to make sure it was called.
+            // We could create a stub here as well and it will be called,
+            // but using a stub means the actual filter function doesn't get
+            // called upon which would make add() fail.
+            var filterSpy = sinon.spy(this.calculator, '_filter');
+            this.calculator.substract(2, 5);
             // Chai + Sinon here.
             expect(filterSpy).to.have.been.called;
             // When creating a spy or stub to wrap a function, you'll want
